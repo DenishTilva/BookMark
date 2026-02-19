90c602fcb219ce2ca67d4f7cb1347fae9a9b1aec
DenishTilva <pateldenish5646@gmail.com>
2026-02-19 11:37:34 +0530

    build error

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config, { isServer }) => {
+    // Disable persistent cache to fix serialization warning
+    config.cache = false;
+    return config;
+  },
 };
 
 export default nextConfig;
